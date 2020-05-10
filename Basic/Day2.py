b591c688a17146176a3f81950373c0b803a02834
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-05-10 05:59:58 +0530

    Learning Python Basics

---
+++ Basic/Day2.py
@@ -0,0 +1,11 @@
+import math;
+print ("DAY-2")
+#always stores a string
+#need to be type casted
+radius = int(input("Enter Radius: ")) 
+area = math.pi*math.pow(radius,2)
+perimeter = 2*math.pi*radius
+print("Area:", end=" ")
+print(area)
+print("Perimeter:", end=" ")
+print(perimeter)
