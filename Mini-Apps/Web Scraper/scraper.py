644c26d9687f3b8734e3b27c77a6f19e8d610094
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-05-22 10:13:55 +0530

    Scraping links from https://news.google.com and writing on a txt file

--- Mini-Apps/Web Scraper/scraper.py
+++ Mini-Apps/Web Scraper/scraper.py
@@ -1,5 +1,5 @@
 import urllib.request
-from bs4 import beautifulsoup
+from bs4 import BeautifulSoup
 
 class Scraper:
 	def __init__(self,site):
@@ -7,12 +7,15 @@ class Scraper:
 
 	def scrape(self):
 		r = urllib.request.urlopen(self.site)
+		f = open('links.txt','a')
 		html = r.read()
 		parser = "html.parser"
-		sp = beautifulsoup(html, parser)
+		sp = BeautifulSoup(html, parser)
 		for tag in sp.find_all("a"):
 			url = tag.get("href")
-			print("\n"+url)
+			print("\n",url)
+			f.write(url+"\n\n")
+		f.close()
 
 scrape = Scraper("https://news.google.com")
 scrape.scrape()	
