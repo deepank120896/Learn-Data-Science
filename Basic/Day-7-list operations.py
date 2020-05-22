293802cd51ac39eaf2e8662337a31f18e29b7f96
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-05-22 10:24:16 +0530

    List Operations

--- Basic/Day-7-list operations.py
+++ Basic/Day-7-list operations.py
@@ -1,3 +1,35 @@
+# Consider a list (list = []). You can perform the following commands:
+
+# Input Format
+# The first line contains an integer, denoting the number of commands.
+# Each line  of the  subsequent lines contains one of the commands.
+
+# Constraints
+# The elements added to the list must be integers.
+
+# Output Format
+# For each command of type print, print the list on a new line.
+
+# Sample Input 0
+# 12
+# insert 0 5
+# insert 1 10
+# insert 0 6
+# print
+# remove 6
+# append 9
+# append 1
+# sort
+# print
+# pop
+# reverse
+# print
+
+# Sample Output 0
+# [6, 5, 10]
+# [1, 5, 9, 10]
+# [9, 5, 1]
+
 if __name__ == '__main__':
     N = int(input())
 
